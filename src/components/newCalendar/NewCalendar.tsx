da0297edf46220550c153eb8e2ecb9c968ba5bbb
Daniil Zagumennii <danielzagumennyi@gmail.com>
2022-11-01 21:56:50 +0200

    update

--- src/components/newCalendar/NewCalendar.tsx
+++
@@ -1,4 +0,0 @@
-export type {
-  ICalendarHoverRangeType,
-  ICalendarPickerType
-} from "./components/Calendar";
--- src/components/newCalendar/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const YEAR_GROUP_COUNT = 10;
-export const MONTH_GROUP_COUNT = 12;
